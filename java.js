6f8230f33e15eb94da745d61039e32b675918ae7
Sebastian <sebastian.stromberg@skola.taby.se>
2023-05-11 21:51:44 +0200

    api fixat

--- java.js
+++ java.js
@@ -3,7 +3,7 @@
 
 const block = document.querySelector(".grid-container")
 
-for (let index = 0; index < 100; index++) {
+for (let index = 0; index < 0; index++) {
     block.innerHTML += `<div class="celeb-block"></div>`
     
 }
@@ -27,6 +27,59 @@ function slider() {
 }
 
 /**/
+
+/***      ALL OF THAT API SHIT           ***/
+
+const apiKey = "1a08c634ec1bc9d64558c15c3e88cdbf";
+const baseUrl = "https://api.themoviedb.org/3/person/popular";
+const totalPages = 5; // set the total number of pages you want to retrieve
+let page = 1;
+
+const fetchActors = () => {
+  const url = `${baseUrl}?api_key=${apiKey}&page=${page}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const actors = data.results;
+      actors.forEach(actor => {
+        const celebBlock = document.createElement("div");
+        celebBlock.className = "celeb-block";
+
+        const img = document.createElement("img");
+        img.src = `https://image.tmdb.org/t/p/w500/${actor.profile_path}`;
+
+        // remove the celeb block if the image fails to load or cannot be fetched
+        img.onerror = () => {
+          celebBlock.remove();
+        };
+
+        const nameDiv = document.createElement("div");
+        nameDiv.className = "name-div";
+        nameDiv.innerHTML = actor.name;
+
+        celebBlock.appendChild(img);
+        celebBlock.appendChild(nameDiv);
+        gridContainer.appendChild(celebBlock);
+      });
+
+      if (page < totalPages) {
+        page++;
+        fetchActors();
+      }
+    })
+    .catch(error => {
+      console.error("Error: Could not retrieve actor data", error);
+    });
+};
+
+const gridContainer = document.createElement("div");
+gridContainer.className = "grid-container";
+document.body.appendChild(gridContainer);
+
+fetchActors();
+
+/*** ***/
 
 ///***      USER POP UP     ***///
 
@@ -68,7 +121,7 @@ function adpop() {
   }
 }
 
-setInterval(adpop, 120000);
+setInterval(adpop, 360000000);
 
 /**/
 
@@ -83,3 +136,29 @@ let closebutton = document.querySelector(".close-button")
 closebutton.addEventListener("click", adpopremove)
 
 /**/
+
+///*** creates a search result pop up  ***///
+
+let searchbar = document.querySelector("#search")
+
+let resultpopup = document.querySelector(".search-requests-block")
+
+let searchButton = document.querySelector(".searchButton")
+
+
+
+
+/* makes keypress ENTER search */
+
+// searchbar.addEventListener("keypress", a); {
+//   if (a.key === "Enter") {
+//     search_result_popup()
+//   }
+// }
+
+
+// document.getElementById("searchButton").onclick = search_result_popup()
+
+// function search_result_popup() {
+//   resultpopup.setAttribute("id", "search-request-appear")
+// }
